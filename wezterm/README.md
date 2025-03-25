990d5c2e230ac0f5e2fa12b167241671912e7b25
Samuele-Paglia <43786670+Samuele-Paglia@users.noreply.github.com>
2025-03-25 14:02:49 +0100

    wezterm terminal emulator (#8)
    Adding wezterm terminal emulator with its configuration file. This close
    issue #4

---
+++ wezterm/README.md
@@ -0,0 +1,14 @@
+# Installation
+
+MacOS' wezterm installation:
+
+```bash
+brew install --cask wezterm
+```
+The wezterm configuration file is written in Lua.
+
+To list all system installed fonts, wezterm provides the following command:
+
+```bash
+wezterm ls-fonts --list-system
+```
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,23 @@
+-- Use wezterm API
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+-- Appearance
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+config.color_scheme = "Catppuccin Mocha"
+config.window_background_opacity = 0.9
+--config.macos_window_background_blur = 10
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+-- Font
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 12
+--config.line_height = 1.2 -- Not applied for a bug, introduces unexpected bottom padding
+
+return config

c567007a99458cc0d211f7e925ab1442fea38d95
Samuele Paglia <sml.pgl@gmail.com>
2025-10-08 23:55:29 +0200

    chore(emacs): adding early-init as tangled file

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,5 +1,1 @@
-;; This file get executed before the init itself.
-
-;; Disabling the default Emacs package manager to
-;; not conflict with the alpaca package manager
 (setq package-enable-at-startup nil)
